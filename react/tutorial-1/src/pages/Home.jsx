589a47b23ad9dc701b6938c64c0d034a1430dadb
yodanisesutantio <yodanise66@gmail.com>
2025-05-04 08:37:41 +0700

    feat: use react click events, and lo and behold useState hook

--- react/tutorial-1/src/pages/Home.jsx
+++ react/tutorial-1/src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [name, setName] = useState("Bianca");
+  const [age, setAge] = useState(12);
+
+  const handleClick = () => {
+    setName("Iris");
+    setAge(14);
+  };
+
   return (
     <>
       <div className="home">
         <h2>Pokedex List</h2>
+        <p>
+          {name} is {age} years old.
+        </p>
+        <button onClick={handleClick}>Click Me!</button>
       </div>
     </>
   );
