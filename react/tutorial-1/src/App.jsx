dbc0429d8809448f89a68c57074de511f786691b
yodanisesutantio <yodanise66@gmail.com>
2025-05-05 23:25:28 +0700

    feat: understanding and managing react router and native react <Link>, also imported react-router-dom at the same time created new pages component to add new data

--- react/tutorial-1/src/App.jsx
+++ react/tutorial-1/src/App.jsx
@@ -1,14 +1,21 @@
 // import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Create from "./pages/Create";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<Create />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- react/tutorial-1/src/components/Navbar.jsx
+++ react/tutorial-1/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -7,8 +8,8 @@ const Navbar = () => {
         <h1>Gen 5 Pokedex</h1>
 
         <div className="links">
-          <a href="/">Home</a>
-          <a href="/add">Add New Pokemon</a>
+          <Link to="/">Home</Link>
+          <Link to="/add">Add New Pokemon</Link>
         </div>
       </nav>
     </>
