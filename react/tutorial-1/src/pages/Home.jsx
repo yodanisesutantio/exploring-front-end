854338ce8063ee84f88b1275e6e2ccf43be4239d
yodanisesutantio <yodanise66@gmail.com>
2025-05-05 22:52:18 +0700

    feat: conditional templating and error and of course any other message

--- react/tutorial-1/src/pages/Home.jsx
+++ react/tutorial-1/src/pages/Home.jsx
@@ -3,26 +3,42 @@ import PokemonList from "../components/PokemonList";
 
 const Home = () => {
   const [pokemons, setPokemons] = useState(null);
-
-  const [name, setName] = useState("Bianca");
+  // const [name, setName] = useState("Bianca");
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8000/pokemons")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        // console.log(data);
-        setPokemons(data);
-      });
-  }, [name]);
+    setTimeout(() => {
+      fetch("http://localhost:8000/pokemons")
+        .then((res) => {
+          // console.log(res);
+          if (!res.ok) {
+            throw Error("Could not reach data source");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          // console.log(data);
+          setPokemons(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          // console.log(err.message);
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <>
       <div className="home">
+        {isPending && <div>Loading...</div>}
+        {error && <div>{error}</div>}
         {pokemons && <PokemonList pokemons={pokemons} title="Pokedex Lists" />}
-        <button onClick={() => setName("Cheren")}>Change Name</button>
-        <p>{name}</p>
+        {/* <button onClick={() => setName("Cheren")}>Change Name</button>
+        <p>{name}</p> */}
       </div>
     </>
   );
