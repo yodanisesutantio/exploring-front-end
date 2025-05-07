2960b2b8ea0215693f71f969222c68af7be64de0
yodanisesutantio <yodanise66@gmail.com>
2025-05-07 11:26:58 +0700

    feat: implemented simplegrid component and grid component as well as grid item, and attempting to make them responsive

--- react/tutorial-2/src/layouts/RootLayout.jsx
+++ react/tutorial-2/src/layouts/RootLayout.jsx
@@ -1,11 +1,28 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
+    <Grid templateColumns={"repeat(6, 1fr)"} backgroundColor={"whiteAlpha.200"}>
+      <GridItem
+        as={"aside"}
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        backgroundColor={"purple.400"}
+        minHeight={{ lg: "100vh" }}
+        padding={{ base: 3, xl: 7 }}
+      >
+        <span>Sidebar</span>
+      </GridItem>
+      <GridItem
+        as={"main"}
+        colSpan={{ base: 6, lg: 4, xl: 5 }}
+        paddingX={{ base: 6, xl: 10 }}
+        paddingY={{ base: 2, xl: 4 }}
+      >
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 }
--- react/tutorial-2/src/pages/Dashboard.jsx
+++ react/tutorial-2/src/pages/Dashboard.jsx
@@ -1,25 +1,72 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <Container maxWidth={"2xl"} as={"section"} paddingY={"5"}>
-      <Heading marginY={"30px"} padding={"10px"}>
-        Chakra UI Component
-      </Heading>
-      <Text marginLeft={"30px"}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, non?
-      </Text>
+    <SimpleGrid padding={"2.5"} columns={4} gap={"3"} minChildWidth={"250px"}>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
 
       <Box
-        padding={"10"}
-        backgroundColor={"teal.700"}
-        color={"white"}
-        textAlign={"center"}
-      >
-        Hello, Trainers!
-      </Box>
-    </Container>
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+      <Box
+        backgroundColor={"black"}
+        height={"200px"}
+        border={"1px solid white"}
+      ></Box>
+    </SimpleGrid>
   );
 };
 
