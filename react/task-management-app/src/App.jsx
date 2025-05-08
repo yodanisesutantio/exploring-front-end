726e8f7547916a67855d4556a7577b2b84000429
yodanisesutantio <yodanise66@gmail.com>
2025-05-09 06:34:02 +0700

    feat: created new react project and installed chakra UI and react-router-dom package for start

---
+++ react/task-management-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button, HStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </>
+  );
+}
+
+export default App;
