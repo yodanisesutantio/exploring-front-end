39bad41569bf933ba1ddac761a7fe85102c8bfd5
yodanisesutantio <yodanise66@gmail.com>
2025-05-13 21:26:33 +0700

    feat: npm basics, added simple express apps

---
+++ nodejs/tutorial-1/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  //   res.send("<p>Homepage</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
--- nodejs/tutorial-1/server.js
+++ nodejs/tutorial-1/server.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // Lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log("Hello");
+  });
+
+  greet();
+  // greet();
+
   res.setHeader("Content-Type", "text/html");
 
   let path = "./views/";
