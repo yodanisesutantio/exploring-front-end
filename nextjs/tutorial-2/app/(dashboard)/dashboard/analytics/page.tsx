bce0d24f720e8c2d9a706f81aeae3f90c0c0c382
yodanisesutantio <yodanise66@gmail.com>
2025-05-26 04:54:46 +0700

    abandon: the use of nextjs auth is different from the video, so I cant keep up with it

--- nextjs/tutorial-2/app/(dashboard)/dashboard/analytics/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div>Analytics</div>
-  )
-}
-
-export default Page
--- nextjs/tutorial-2/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loading = () => {
-  return (
-    <div>_________</div>
-  )
-}
-
-export default Loading
