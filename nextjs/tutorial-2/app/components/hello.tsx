3311f7938c528535a291c988eed64941345ef287
yodanisesutantio <yodanise66@gmail.com>
2025-05-25 21:06:35 +0700

    feat: new nextjs tutorial

---
+++ nextjs/tutorial-2/app/components/hello.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react'
+
+function Hello() {
+    console.log('I am a client component');
+    
+  return (
+    <div className="">
+        <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Hello
